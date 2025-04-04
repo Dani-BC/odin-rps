53b51602b3155bb1e62abb1c4e99fb1547d6a2eb
Dani-BC <danielbc.personal9@gmail.com>
2025-04-04 10:59:26 +0100

    made it better

--- jscript.js
+++ jscript.js
@@ -1,74 +1,76 @@
-
 function randondNumber(max) {
-    return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max);
 }
 
 function botOption() {
-    let botOption = randondNumber(3);
-    if (botOption == 0) {
-        return "rock";
-    } else if (botOption == 1) {
-        return "paper";
-    } else {
-        return "scissors";
-    }
+  let botOption = randondNumber(3);
+  if (botOption == 0) {
+    return "rock";
+  } else if (botOption == 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
 }
 
-function playerOption(){ 
-    let playerOption = prompt("Enter your option: 1-rock, 2-paper, 3-scissors");
-    if(playerOption == 1) {
-        playerOption = "rock";
-        return playerOption;
-    }else if(playerOption == 2) {
-        playerOption = "paper";
-        return playerOption;
-    }else if(playerOption == 3) {
-        playerOption = "scissors";
-        return playerOption;
-    }
-    console.log("Player option: " + playerOption);
+function playerOption() {
+  let playerOption = prompt("Enter your option: 1-rock, 2-paper, 3-scissors");
+  if (playerOption == 1) {
+    playerOption = "rock";
+    return playerOption;
+  } else if (playerOption == 2) {
+    playerOption = "paper";
+    return playerOption;
+  } else if (playerOption == 3) {
+    playerOption = "scissors";
+    return playerOption;
+  }
+  console.log("Player option: " + playerOption);
 }
 
-function game(){
+function game() {
+  let rounds = 5;
+  let playerScore = 0;
+  let botScore = 0;
+
+  do {
     let playeropt = playerOption();
     let botopt = botOption();
     console.log("Player option: " + playeropt);
     console.log("Bot option: " + botopt);
 
-    if(playeropt == "rock" && botopt == "scissors"){
-        console.log("Player wins!")
-        playagain();
-    }else if(playeropt == "rock" && botopt == "paper"){
-        console.log("Bot wins!")
-        playagain();
-    }else if(playeropt == "paper" && botopt == "rock"){
-        console.log("Player wins!")
-        playagain();
-    }else if(playeropt == "paper" && botopt == "scissors"){
-        console.log("Bot wins!")
-        playagain();
-    }else if(playeropt == "scissors" && botopt == "paper"){
-        console.log("Player wins!")
-        playagain();
-    }else if(playeropt == "scissors" && botopt == "rock"){
-        console.log("Bot wins!")
-        playagain();
-    }else{
-        console.log("Draw!")
-        playagain();
+    if (playeropt == "rock" && botopt == "scissors") {
+      console.log("Player wins!");
+      playerScore++;
+    } else if (playeropt == "rock" && botopt == "paper") {
+      console.log("Bot wins!");
+      botScore++;
+    } else if (playeropt == "paper" && botopt == "rock") {
+      console.log("Player wins!");
+      playerScore++;
+    } else if (playeropt == "paper" && botopt == "scissors") {
+      console.log("Bot wins!");
+      botScore++;
+    } else if (playeropt == "scissors" && botopt == "paper") {
+      console.log("Player wins!");
+      playerScore++;
+    } else if (playeropt == "scissors" && botopt == "rock") {
+      console.log("Bot wins!");
+      botScore++;
+    } else {
+      console.log("Draw!");
     }
-}
 
-function playagain() {
-    let playagain = prompt("Do you want to play again? 1-yes, 2-no");
+    rounds--;
+  } while (rounds > 0);
 
-    if(playagain == 1){
-        game();
-    }
-    else{
-        console.log("Game ended")
-    }
-    return 0;
+  if (playerScore > botScore) {
+    console.log("Player wins the game!");
+  } else if (botScore > playerScore) {
+    console.log("Bot wins the game!");
+  } else {
+    console.log("It's a draw!");
+  }
 }
 
 console.log(game());
